3e39a1af16260ac3a6aa0b0294a5f5cf47cc06d4
Leandro C <lecona28@gmail.com>
2019-09-16 12:24:08 -0600

    Version #1 - NodeJS, Express, Routes, FrontEnd, MongoDB

---
+++ v1/app/server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+
+var app = express();
+
+var server = app.listen(3000, () => {
+    console.log('server is running on port', server.address().port);
+   });
+
+app.use(express.static(__dirname));
+
+var mongoose = require('mongoose');
+
+var dbUrl = 'mongodb+srv://lcordero:P4ssw0rd@progra3-oh8l9.mongodb.net/test?retryWrites=true&w=majority'
+
+mongoose.connect(dbUrl , (err) => { 
+    console.log('mongodb connected',err);
+ })
+
+ var Todo = mongoose.model('Todo',{ name : String, description : String})
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+app.get('/todos', (req, res) => {
+    Todo.find({},(err, todos)=> {
+      res.send(todos);
+    })
+  })
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo(req.body);
+    todo.save((err) =>{
+      if(err)
+        sendStatus(500);
+      res.sendStatus(200);
+    })
+  })
