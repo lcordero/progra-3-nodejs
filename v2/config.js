87bc4d5edbf40837564ed96b1413f6b20f737732
Jairo1123 <jcvv8607@yahoo.es>
2019-10-30 20:30:58 -0600

    product

---
+++ v2/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  port: process.env.PORT || 3001,
+  db: process.env.MONGODB_URI || 'mongodb+srv://jairo11:stefanny11@cluster0-orxmw.mongodb.net/test/product',
+  SECRET_TOKEN: 'miclavedetokens'
+}
+
