fa2463583dc77e7cb383fc5bc0085071b6838748
Chonsos <red_alonso_2001@hotmail.com>
2019-11-14 04:27:54 +0100

    Practica v3

--- v3/config.js
+++ v3/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3001,
-  db: process.env.MONGODB_URI || 'mongodb+srv://lcordero:P4ssw0rd@progra3-oh8l9.mongodb.net/test',
+  db: process.env.MONGODB_URI || 'mongodb+srv://alonso-v:Sito1199@cluster0-hunby.mongodb.net/v3?retryWrites=true&w=majority',
   SECRET_TOKEN: 'miclavedetokens'
 }
