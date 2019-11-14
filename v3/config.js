d6210ec80f499681ff6827fcc826c94643ca1631
Katherine <kathi9605@hotmail.com>
2019-11-13 22:42:11 -0600

    practica

--- v3/config.js
+++ v3/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3001,
-  db: process.env.MONGODB_URI || 'mongodb+srv://lcordero:P4ssw0rd@progra3-oh8l9.mongodb.net/test',
+  db: process.env.MONGODB_URI || 'mongodb+srv://katherine-g:Kakaka111@cluster0-4vspk.mongodb.net/test?retryWrites=true&w=majority',
   SECRET_TOKEN: 'miclavedetokens'
 }
