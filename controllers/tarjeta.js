f1523f3acabf75f7b117322420000a1a76ee5f9b
Jairo1123 <jcvv8607@yahoo.es>
2019-11-25 00:00:29 -0600

    extra

--- controllers/tarjeta.js
+++ controllers/tarjeta.js
@@ -3,7 +3,7 @@ const Tarjeta = require('../models/tarjeta')
 
 
 function getTarjetas (req, res) {
-    let tarjetaId = req.params.tarjetaId
+    let tarjeta = req.params.tarjeta
     Tarjeta.find({}, (err, tarjeta) => {
         if (err) return res.status(500).send({message: `Error al realizar la petición: ${err}`})
         if (!tarjeta) return res.status(404).send({message: `La tarjeta no esta`})
@@ -19,18 +19,15 @@ function saveTarjeta (req, res) {
     tarjeta.name = req.body.name
     tarjeta.color = req.body.color
     
-    tarjeta.save((err, tarjetaStored) => {
-        if (err) res.status(500).send({message: `Error al salvar en la base de datos: ${err} `})
-    
-    res.status(200).send({ tarjeta: tarjetaStored })
-})
+       res.status(200).send( 'tarjeta guardada' )
 }
 
+
 function updateTarjeta (req, res) {
-    let tarjetaId = req.params.tarjeta
+    let tarjeta = req.params.tarjeta
     let update = req.body
 
-    Tarjeta.findByIdAndUpdate(tarjetaId, update, (err, tarjetaUpdated) => {
+    Tarjeta.findByIdAndUpdate(tarjeta, update, (err, tarjetaUpdated) => {
         if (err) res.status(500).send({message: `Error al actualizar esta tarjeta: ${err}`})
     
     console.log("aqui actualizo")
